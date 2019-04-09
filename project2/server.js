a8f4b563318a4971a59cdf9152eca19fbe2f735f
lovdeepm <lovdeep@gmail.com>
2019-04-08 23:46:20 -0700

    added some passport info

--- project2/server.js
+++ project2/server.js
@@ -1,16 +1,28 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var env = require('dotenv').load();
 var db = require("./models");
 
 var app = express();
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
 var PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true}));// session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
 // Handlebars
 app.engine(
@@ -26,6 +38,20 @@ require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: true };
+
+//Models
+var models = require("./models");
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+console.log(err, "Something went wrong with the Database Update!")
+ 
+});
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
