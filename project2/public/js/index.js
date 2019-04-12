947d75904e112a221da849333b3d5580e8e3dbe9
Jason Cartwright <jason.cartwright44@gmail.com>
2019-04-11 21:32:51 -0700

    button

--- project2/public/js/index.js
+++ project2/public/js/index.js
@@ -97,3 +97,9 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+$("#add").on("click", function() {
+  // var newPick = {
+  //   name: this.name
+  // }
+});
